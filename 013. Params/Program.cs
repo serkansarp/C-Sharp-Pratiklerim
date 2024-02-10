cd1e5b4d58359f9218e8d4195a2b9db874c1b5b7
serkansarp <serkansarp79@gmail.com>
2024-02-10 13:58:42 +0300

    params project added

---
+++ 013. Params/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ 013. Params
+•   Fonksiyonları oluştururken alacakları değişken tip, sayı ve/veya direkt değerin kendisini giriyoruz
+    bildiğimiz üzere. Bu değerleri sınırsız yapmak için params kullanıyoruz.
+•   Biz static int topla(params int[] sayilar) şeklinde fonksiyonu başlattık, birden çok sayı gelebileceği
+    için dizi gösterdik ve adını sayilar koyduk.
+•   Fonksiyon içerisine de gelen bütün değerleri toplayan bir foreach döngüsü yazıp toplamı geri döndürdük.
+ */
+
+namespace _013.Params
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fonksiyonun döndürdüğü değeri burada yazdırıyoruz
+            Console.WriteLine(Topla(5, 8, 14, 21)); 
+
+            Console.ReadKey();
+        }
+        
+        // Fonksiyon int döndürüyor, toplam=0 ile yeni değişken yaratıp sonuçta onu döndürüyoruz
+        static int Topla(params int[] sayilar)
+        {
+            int toplam = 0;
+            foreach (int sayi in sayilar)
+            {
+                toplam += sayi;
+            }
+            return toplam;
+        }
+
+    }
+
+    
+
+}
