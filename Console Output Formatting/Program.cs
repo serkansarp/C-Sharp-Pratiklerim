ae767622a6c098e2aa0494d48aae45addfdae9e3
serkansarp <serkansarp79@gmail.com>
2024-04-06 23:34:55 +0300

    Output Types project just created.

---
+++ Console Output Formatting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Output_Formatting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 81;
+            Console.Write("Sayi={0,10:F2}",sayi);
+            Console.Write(" - Console.Write(\"Sayi={0,10:F2}\",sayi) koduyla toplam 10 satır yazmasını sağladık.");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
