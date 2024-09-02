1f5c36e8753cd9995cada766ea5dfae850733f33
serkansarp <serkansarp79@gmail.com>
2024-09-02 22:07:34 +0300

    New practices added

---
+++ 024. Genişletme Metodları - Extention Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _024.Genişletme_Metodları___Extention_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "selaM, nasIlSın?";
+            Console.WriteLine("Orijinali:\n"+a);
+            a = a.IlkHarfiBuyut();
+            Console.WriteLine("Düzenlenmişi:\n"+a);
+            
+
+            Console.ReadKey();
+        }
+    }
+
+    static class StringGenisletme    // Static sınıf içinde olmalı ve bu yüzden tüm üyeleri de static.
+    {
+        public static string IlkHarfiBuyut(this string a)   // this ve public static ile yazılmalıdır.
+        {
+            if (string.IsNullOrEmpty(a))    // Stringin boş olup olmadığına bakıp boşsa kendisini geri döndürür.
+                return a;
+            else
+                return char.ToUpper(a[0]) + a.Substring(1).ToLower(); // İlk harfi büyütüp gerisini küçültüp, birleştirip döndürür.
+            
+        }
+    }
+}
---
+++ 025. Method Overriding/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _025.Method_Overriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kedi duman = new Kedi();
+            duman.Ses();
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Hayvan
+    {
+        // Ana metod 'virtual' ile belirtilir
+        public virtual void Ses()
+        {
+            Console.WriteLine("-");
+        }
+    }
+
+    class Kopek:Hayvan
+    {
+        // Ezilen metod ise override ile belirtilir.
+        // Not: Daha önce 'Metod Gizleme' konusunda new keywordü ile benzer bir pratiğimiz oldu.
+        // Aralarındaki fark polymorphism pratiğinde işledik.
+        public override void Ses()
+        {
+            Console.WriteLine("Hav hav");
+        }
+    }
+
+    class Kedi:Hayvan
+    {
+        public override void Ses()
+        {
+            Console.WriteLine("Miyav");
+        }
+    }
+
+}
