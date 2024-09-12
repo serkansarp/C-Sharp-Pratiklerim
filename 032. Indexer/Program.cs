d8dc3c809f4a6912faa629b5323db83b9d5f1466
serkansarp <serkansarp79@gmail.com>
2024-09-12 22:52:53 +0300

    New practice added

---
+++ 032. Indexer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _032.Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyCollection a = new MyCollection();
+            a[3] = 5;
+            Console.WriteLine(a[3]);    // Set ederken (x+2)*3 dediğimiz için (5+2)*3=21 çıktısını alacağız.
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MyCollection
+    {
+        private int[] data = new int[10];
+
+        // Indexer tanımı, property olarak ve this ile yazıldığına dikkat edelim.
+        public int this[int index]
+        {
+            get { return data[index]; }
+            set { data[index] = (value+2)*3; }  // Set ederken 2 ekleyip toplamın 3 katını set ettik.
+        }
+    }
+
+}
