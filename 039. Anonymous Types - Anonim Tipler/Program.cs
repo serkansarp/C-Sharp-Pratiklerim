34f06a2ac111b223a83b94a24e47e568a8787b6d
serkansarp <serkansarp79@gmail.com>
2024-09-29 19:37:53 +0300

    new practices added.

---
+++ 039. Anonymous Types - Anonim Tipler/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+- Anonymous Types - Anonim Tipler
+• 'Sınıf obje = new Sınıf()' şeklinde sınıftan nesne oluştuluyor, biliyoruz, tip anonim olacağı için
+  'var obje = new { isim = "ahmet", yas=15 } şeklinde oluşturabiliriz.
+• Özellikleri yazdırabilir ama değiştiremeyiz. Get'leri vardır, set'leri yoktur yani.
+• Nested mantığı da desteklerler. Örnekte anonim içindeki detaylar tipi ile gösterdim.
+*/
+namespace _039.Anonymous_Types___Anonim_Tipler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // anonim isimli bir tür oluşturup içine özelliklerini atadık.
+            // Fark ettiyseniz özelliklerin de başında int, string vb. yok, derleyici kendi karar verecek.
+            // detaylar adı altında yeni bir inner tip yaratıp iki özellik de oraya ekledik.
+            var anonim = new { isim = "Selim", yas = 35, kilo = 76.5, detaylar = new { sacRengi = "Kumral", gozuMaviMi = false } };
+
+            // Tip ve değerlerini yazdıralım
+            Console.WriteLine(anonim.isim.GetType() + ":\t" + anonim.isim);  // String yaptı
+            Console.WriteLine(anonim.yas.GetType() + ":\t" + anonim.yas);    // Int32 yaptı
+            Console.WriteLine(anonim.kilo.GetType() + ":\t" + anonim.kilo);  // Double yaptı
+
+            // Inner tipin iç tipleri ve değerleri ise:
+            Console.WriteLine(anonim.detaylar.sacRengi.GetType() + ":\t" + anonim.detaylar.sacRengi);
+            Console.WriteLine(anonim.detaylar.gozuMaviMi.GetType() + ":\t" + anonim.detaylar.gozuMaviMi);   // Bool yaptı
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 039. Anonymous Types - Anonim Tipler/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("039. Anonymous Types - Anonim Tipler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("039. Anonymous Types - Anonim Tipler")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7a27e52b-1384-4d76-9ea9-707fecfac81b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
