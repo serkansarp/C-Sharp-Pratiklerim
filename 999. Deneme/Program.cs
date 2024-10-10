cacdac547eae85510965b569ba69248bc527ed11
serkansarp <serkansarp79@gmail.com>
2024-10-10 17:37:53 +0300

    to do and test practices

---
+++ 999. Deneme/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _999.Deneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Microsoft SQL Express'e bağlantı kur
+            // - Veri çekip göster
+            // - Düzenli yap, sayısını uzunluğunu düzenle.
+            // - SQLite ile ekleme çıkarma, bağlantı, gösterme vb.
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 999. Deneme/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("999. Deneme")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("999. Deneme")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f29d1d27-3201-4135-bdb3-4d73ae6c0aa3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
